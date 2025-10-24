7ba391e848292bd40b2dfa19978b58dda4848fae
小爬的老粉丝 <727842003@qq.com>
2025-10-24 11:13:47 +0800

    fix: when transfer uid absent, set the defaults

--- parse.go
+++ parse.go
@@ -215,7 +215,10 @@ func NewParser(in io.Reader, bytesToRead int64, frameChannel chan *frame.Frame, 
 	}
 	// TODO(https://github.com/wybaby168/dicom/issues/329): consider trying
 	// deflated parsing as a fallback as well.
-	return &p, errors.New("dicom missing transfer syntax uid in metadata, and it was not possible to successfully infer it using the next 100 bytes of the dicom")
+	// Set default transfer syntax when unable to infer it
+	debug.Log("WARN: could not find transfer syntax uid in metadata and unable to infer it, using default Implicit VR Little Endian")
+	p.SetTransferSyntax(binary.LittleEndian, true)
+	return &p, nil
 }
 
 func canReadElementFromBytes(buf []byte, optSet parseOptSet, bo binary.ByteOrder, implicit bool) bool {
